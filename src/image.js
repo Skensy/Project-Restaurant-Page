08ea21173e8490e4a3d48a200fedbb51e992833c
Skensy <11029252+Skensy@users.noreply.github.com>
2024-10-22 11:09:26 +0200

    makes some edits

--- src/image.js
+++ src/image.js
@@ -2,3 +2,5 @@ import restaurant from "./images/restaurant.jpg"
 
 export const image = document.createElement("img");
 image.src = restaurant;
+image.style.width = "auto";
+image.style.height = "200px";
